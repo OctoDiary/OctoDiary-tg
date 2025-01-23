e57fea624c4ec79ea67084237f675c6fe5e0e95e
Den4ikSuperOstryyPer4ik <oleynik.denis.2009@gmail.com>
2025-01-23 23:48:06 +0300

    Add router for `admins` commands

--- core/routers.py
+++ core/routers.py
@@ -10,12 +10,14 @@ from core.handlers.settings import router as settings_router
 from core.handlers.inline import router as inline_router
 from core.handlers.feedback import router as feedback_router
 from core.handlers.auth import router as auth_router
+from core.handlers.admins import router as admins_router
 from core.misc.loops import router as loops_router
 
 from core.handlers import exceptions
 
 routers = [
     start_router,
+    admins_router,
     auth_router,
     diary_router,
     feedback_router,
